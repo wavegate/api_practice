d8003f333354778d825e6a7d40c2cdfd2c31e9cf
wavegate <cc.frankee@gmail.com>
2022-06-17 22:15:56 -0700

    First submission

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # API Practice
 
-Purpose of this project is to practice asynchronous JavaScript, making use of Pok&eacute;API.
+View demo: https://famous-palmier-72762f.netlify.app/
+
+The goal of this project is to practice asynchronous JavaScript using the pokemon API.
+
+This demo uses three different notations to obtain lists of Pokemon from the PokeAPI: AJAX, Promise, and await/async and presents how long it took for the request to occur.
+
+Concepts practiced:
+
+- API GET requests with fetch API and AJAX XMLHttpRequest with error handling
+- Self-invoking async functions
+- Cache prevention by adding a random token to the request
+- Use of regexp to check if a string links to an image (not very effective since only checks if something is an image by name)
+- Scroll into view on added element
+- Navbar padding offset with JavaScript
+- Variable number of parameters and use of 'arguments'
+- Using Date to track how long a process takes
---
+++ script.js
@@ -0,0 +1,112 @@
+const test = document.getElementById("test");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const search = document.getElementById("search");
+const submit = document.getElementById("submit");
+const searchForm = document.getElementById("searchForm");
+const body = document.getElementsByTagName("body")[0];
+const log = document.getElementById("log");
+const logState = log.innerHTML;
+
+const nav = document.getElementById("nav");
+
+function print(stuff) {
+    const newElement = document.createElement("div");
+    newElement.innerHTML = stuff;
+    log.appendChild(newElement);
+}
+
+function printTable() {
+    const newRow = document.createElement("tr");
+    for (let arg of arguments) {
+        const cell = document.createElement("td");
+        if (arg.search(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g) != -1) {
+            cell.innerHTML = `<img src="${arg}">`;
+        } else {
+            cell.innerHTML = arg;
+        }
+        newRow.appendChild(cell);
+    }
+    log.appendChild(newRow);
+    newRow.scrollIntoView();
+}
+
+function printImage(stuff) {
+    const newElement = document.createElement("img");
+    newElement.src = stuff;
+    log.appendChild(newElement);
+}
+
+function resetLog() {
+    log.innerHTML = logState;
+}
+
+function searchPokemon(name, method) {
+    if (name == "") {
+        print("Please enter Pokemon name.");
+        return;
+    }
+    let searchString = `https://pokeapi.co/api/v2/pokemon/${name}/`;
+    if (document.getElementById("uncache").checked == true) {
+        searchString = searchString + "?token=" + Math.floor(Math.random() * 100001);
+    }
+    let startDate;
+    let endDate;
+    switch (method) {
+        case "ajax":
+            startDate = new Date();
+            const request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 404) {
+                    print("Pokemon not found.");
+                } else if (this.readyState == 4 && this.status == 200) {
+                    endDate = new Date();
+                    printTable(JSON.parse(this.response).sprites.front_default, `${endDate - startDate} ms`);
+                }
+            }
+            request.open("GET", searchString);
+            request.send();
+            break;
+        case "promise":
+            startDate = new Date();
+            const fetchPromise = fetch(searchString);
+            fetchPromise.then((response) => {
+                return response.json();
+            }).catch(error => {
+                print("Pokemon not found.")
+            }).then((data) => {
+                endDate = new Date();
+                printTable(data.sprites.front_default, `${endDate - startDate} ms`);
+            });
+            break;
+        case "await":
+            startDate = new Date();
+            (async function () {
+                try {
+                    const response = await fetch(searchString);
+                    const data = await response.json();
+                    endDate = new Date();
+                    printTable(data.sprites.front_default, `${endDate - startDate} ms`)
+                } catch (err) {
+                    print("Pokemon not found.");
+                }
+            })();
+            break;
+        default:
+            return;
+    }
+}
+
+document.getElementById("ajax").addEventListener("click", () => {
+    searchPokemon(search.value, "ajax");
+});
+
+document.getElementById("promise").addEventListener("click", () => {
+    searchPokemon(search.value, "promise");
+});
+
+document.getElementById("await").addEventListener("click", () => {
+    searchPokemon(search.value, "await");
+});
+
+log.style.paddingTop = `${nav.offsetHeight}px`;
